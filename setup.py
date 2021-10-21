8580fb4047d23c191e36a9821ea22e633f6c5e48
ikeikeikeike <jp.ne.co.jp@gmail.com>
2021-10-22 01:42:18 +0900

    add botocore

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='my-deps-setup-py',
     packages=[],
-    install_requires=['pyYAML>=5.4.0'],
+    install_requires=['pyYAML>=5.4.0', 'botocore>1.21'],
 )
